0d3a941617d117774a044dcd626264be28a70a43
joseluiscontigoentodo <93332295+joseluiscontigoentodo@users.noreply.github.com>
2025-03-17 18:59:30 -0300

    Update inscribe.php

--- inscribe.php
+++ inscribe.php
@@ -35,6 +35,6 @@
         }
     }
     else {
-        header("Location: https://portal.contigoentodo.com/#/login");
+        header("Location: https://6yj76mxxeue4ul7zl6zpc76i3i0mfmjz.lambda-url.us-east-1.on.aws/#/login");
     }
 ?>
